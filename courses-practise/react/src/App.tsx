6f0021439c1bc2353d8b0ae1f5f13f226dd2c7eb
Angie <eguzkia6@gmail.com>
2026-04-11 01:41:30 +0200

    add react test project

---
+++ courses-practise/react/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function Header() {
+return <h1>Hello World</h1>;
+ 
+}
+
+function App() {
+
+ return <Header/>;
+}
+
+export default App
